ed81a843c9c574935e8635b7a2c8311393fb565b
Shyamal Shah <shyamalshah@Shyamals-MacBook-Air.local>
2022-01-23 21:38:52 -0600

    Added Sign In page with signin component along with form input and custom button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shoppage.component';
 import Header from './components/header/header.component';
+import SignInAndRegister from './pages/sign-in-and-register/sign-in-and-register.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndRegister} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-register/sign-in-and-register.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../../components/signin/signin.component";
+import './sign-in-and-register.style.scss';
+
+
+const SignInAndRegister = () => (
+    <div>
+        <SignIn></SignIn>
+    </div>
+)
+export default SignInAndRegister;
