ec9f74f484dd38325d311bae58285403a990bd1b
Shyamal Shah <shyamalshah@Shyamals-MacBook-Air.local>
2022-02-06 12:48:44 -0600

    Update cart icon component to count total item into cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,13 +4,16 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { connect } from "react-redux";
 import './cart-icon.style.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden , itemCount}) => (
     <div className="cart-icon">
         <ShoppingIcon className="shopping-icon" onClick={toggleCartHidden}></ShoppingIcon>
-        <span className="item-count"> 0 </span>
+        <span className="item-count">{itemCount}</span>
     </div>
 );
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
-export default connect(null,mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({cart:{addItems}}) => ({
+    itemCount: addItems.reduce((accumalatedQuantity,addItem) => accumalatedQuantity + addItem.quantity,0)
+})
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
